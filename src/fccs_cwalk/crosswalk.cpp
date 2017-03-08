4eea406c33fb2c527de6c5911c0a14ea7aaf3b9f
Kjell Swedin <kjells@uw.edu>
2017-03-07 17:17:00 -0800

    add new exectuable for generating fccs crosswalk data

---
+++ src/fccs_cwalk/crosswalk.cpp
@@ -0,0 +1,17 @@
+//  =============================================================================
+// 
+//  Author:	Kjell Swedin
+//  Purpose:	Generate spreadrate and flamelength data from fuel models
+// 
+//  =============================================================================
+
+#include "fuelModelSet.h"
+#include "behaveRun.h"
+
+
+int
+main(void)
+{
+    
+    return 0;
+}
